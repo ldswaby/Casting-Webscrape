c47a7ef78673b12774845a5b2f36881543580b44
Luke Swaby <lukeswabypetts@hotmail.com>
2021-12-29 14:42:26 +0000

    Can now attach documents

--- Code/send_email.py
+++ Code/send_email.py
@@ -7,6 +7,7 @@ __version__ = '0.0.1'
 
 ## Imports ##
 import re
+import os
 import markdown
 import pandas as pd
 import argparse
@@ -14,15 +15,20 @@ from pwinput import pwinput
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email import encoders
+from email.mime.base import MIMEBase
 import tkinter as tk
 from tkinter import filedialog
 
 
 # TODO: add email attachments signature:
 #  https://realpython.com/python-send-email/#sending-fancy-emails
-# bold = bold{...}
-# italics = italics{...}
-#
+# Add footer to email - would you like to add signature? Y/N  https://stackoverflow.com/questions/60316249/how-to-include-inline-images-in-e-mail-signature-when-sent-out-with-python
+# parse boolean of whether to attach file(s)
+# if yes then askopenfilenames() to select all - need list (if none then empty list)
+# in main(): for file in files: then all code
+
+
 # show example template email before sending
 
 ## Variables ##
@@ -32,6 +38,15 @@ providers = {"gmail": "smtp.gmail.com",
              "icloud": "smtp.mail.me.com"}
 
 ## Functions ##
+
+def yes_no(prompt):
+    x = input(prompt).lower()
+
+    while x not in ['y', 'n']:
+        x = input("Please enter 'y' (yes) or 'n' (no): ").lower()
+
+    return True if x == 'y' else False
+
 def parse_args():
     """
     Parses arguments from the command line
@@ -64,6 +79,19 @@ def parse_args():
     text = filedialog.askopenfilename()
     print(' '*len(text_prompt) + '\033[A' + text)
 
+    # Document add
+    docs_to_add = []
+    doc_add = yes_no("Do you wish to add any documents to this email? ('y'/'n'): ")
+    if doc_add:
+        doc = filedialog.askopenfilename()  # fetch doc
+        docs_to_add.append(doc)
+
+        doc_add = yes_no("Do you wish to add another document? ('y'/'n'): ")
+        while doc_add:
+            doc = filedialog.askopenfilename()  # fetch doc
+            docs_to_add.append(doc)
+            doc_add = yes_no("Do you wish to add another document? ('y'/'n'): ")
+
     root.destroy()  # remove root window
 
     # Parse email info
@@ -71,7 +99,7 @@ def parse_args():
     usn = input(f"Sender {args.provider.title()} Email Address: ")
     pwd = pwinput("Account Password: ")
 
-    return args.provider, data, usn, pwd, subject, text, args.all
+    return args.provider, data, usn, pwd, subject, text, docs_to_add, args.all
 
 def convert_to_html(text):
     """
@@ -104,7 +132,7 @@ def convert_to_plain(text):
 
     return out
 
-def main(provider, data, from_address, password, subject, text, all):
+def main(provider, data, from_address, password, subject, text, docs_to_add, all):
     """
     Function that sedns email to a load of addresses, replacing '-' with their names
     """
@@ -149,6 +177,28 @@ def main(provider, data, from_address, password, subject, text, all):
         # The email client will try to render the last part first
         msg.attach(part1)
         msg.attach(part2)
+
+        # Attach documents
+        for doc in docs_to_add:
+
+            # Open PDF file in binary mode
+            with open(doc, "rb") as attachment:
+                # Add file as application/octet-stream
+                # Email client can usually download this automatically as attachment
+                part = MIMEBase("application", "octet-stream")
+                part.set_payload(attachment.read())
+
+            # Encode file in ASCII characters to send by email
+            encoders.encode_base64(part)
+
+            # Add header as key/value pair to attachment part
+            part.add_header(
+                "Content-Disposition",
+                f"attachment; filename= {os.path.basename(doc)}",
+            )
+
+            # Add attachment to message and convert message to string
+            msg.attach(part)
 
         session.sendmail(from_address, to_address, msg.as_string())  # send email
 
